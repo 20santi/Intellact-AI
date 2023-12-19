280db07f3e3c62e545f0150be9c026a997311de6
Santi Singha <santisingha191@gmail.com>
2023-12-19 13:00:12 +0530

    Create Dashboard page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,8 @@
-import { Button } from '@/components/ui/button'
-import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
 
 export default function DashboardPage() {
   return (
     <div>
         <p>Dashboard page (protected)</p>
-        <UserButton afterSignOutUrl='/'/>
     </div>
   )
 }
