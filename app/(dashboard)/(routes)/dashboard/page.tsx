ef5f2fa7ae843e5787f95fefdb8b314b753311a1
Santi Singha <santisingha191@gmail.com>
2023-12-19 12:40:00 +0530

    Implement Authentication

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+
+export default function DashboardPage() {
+  return (
+    <div>
+        <p>Dashboard page (protected)</p>
+        <UserButton afterSignOutUrl='/'/>
+    </div>
+  )
+}
